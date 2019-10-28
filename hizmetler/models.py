604cee9652decd9e2c757b6d8a2fa262c7418d5e
Mert Çerçiler <mertcerciler@gmail.com>
2019-10-28 19:14:16 +0300

    new

--- hizmetler/models.py
+++ hizmetler/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Hizmet(models.Model):
     baslik = models.CharField(max_length=64)
-    icerik1 = models.TextField(blank=True)
+    icerik1 = models.TextField(blank=True, )
     icerik2 = models.TextField(blank=True)
     resim = models.ImageField(upload_to='')
     resim2 = models.ImageField(upload_to='', blank=True)
