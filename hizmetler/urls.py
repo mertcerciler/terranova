fb232e884fd757551dd9ac01f8925c17be1e4909
Mert Çerçiler <mertcerciler@gmail.com>
2019-11-04 02:28:30 +0300

    Son34

--- hizmetler/urls.py
+++ hizmetler/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hizmet, name='hizmetler'),
+    path('', views.hizmet2, name='hizmetler'),
     path('<int:hizmet_id>', views.hizmet_ayrintili, name='hizmet_ozel')
 ]
--- hizmetler/views.py
+++ hizmetler/views.py
@@ -17,7 +17,24 @@ def hizmet(request):
         'hizmetler' : hizmetler,
         'mylist' : mylist,
     }
-    return render(request, 'hizmetler/hizmetler.html, pages/index.html', context)
+    return render(request, 'hizmetler/hizmetler.html', context)
+def hizmet2(request):
+    hizmetler = Hizmet.objects.all()
+    kisa_icerik = []
+   
+    for hizmet_element in hizmetler:
+        ch = 0
+        str1 = ""
+        for x in range(0,260):
+            str1  = str1 + hizmet_element.icerik1[x]
+        kisa_icerik.append(str1)
+        ch = ch + 1
+    mylist = zip(hizmetler, kisa_icerik)
+    context = {
+        'hizmetler' : hizmetler,
+        'mylist' : mylist,
+    }
+    return render(request, 'pages/index.html', context)
 
 def hizmet_ayrintili(request, hizmet_id):
     hizmet1 = get_object_or_404(Hizmet, pk= hizmet_id)
