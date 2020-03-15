2719cd1f56121a1fdc258640714c2f9ab77e9afc
Mert Çerçiler <mertcerciler@gmail.com>
2020-03-16 02:57:00 +0300

    wp5

--- terranova/settings.py
+++ terranova/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'd1f9hifjr9du%zrc0wu^qyg=(@k)fq8y6buv5&#a!9=k42&+y$'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['165.22.199.218', 'terranovahayat.com', 'www.terranovahayat.com', '127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['terranovahayat.com', ]
 
 # Application definition
 
