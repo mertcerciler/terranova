52731f25306d4e510ada56352b773521aaf5e8b7
Mert Çerçiler <mertcerciler@gmail.com>
2020-03-17 13:15:09 +0300

    wp39

--- terranova/settings.py
+++ terranova/settings.py
@@ -128,6 +128,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/foto')
 MEDIA_URL = 'static/foto/'
 
 try: 
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass 
